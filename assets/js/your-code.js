e1ab3ea9d17ffd759d92311080f84fd62b1e8282
ryansummerlin <ryansummerlin777@gmail.com>
2023-04-05 10:43:10 -0700

    Completed all functions, passed all tests

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,50 @@
 export function getAllDogs() {
-    // Your code here
+    return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+    return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here
+    const url = "/dogs";
+    const headers = {"Content-Type": "application/x-www-form-urlencoded"};
+    const body = new URLSearchParams({
+        name: "Ryan jr",
+        age: 7
+    });
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    };
+
+    return fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+    const url = "/dogs";
+    const headers = {"Content-Type": "application/x-www-form-urlencoded"};
+    const body = new URLSearchParams({
+        name: name,
+        age: age
+    });
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    };
+
+    return fetch(url, options);
 }
 
 export function deleteDog(id) {
-      // Your code here
+    const url = `/dogs/${id}/delete`;
+    const headers = {"AUTH": 'ckyut5wau0000jyv5bsrud90y'};
+    const options = {
+        method: "POST",
+        headers: headers
+    };
+
+    return fetch(url, options);
 }
